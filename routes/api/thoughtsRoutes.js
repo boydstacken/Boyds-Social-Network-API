67906b7365897a86a3827ba4e97243a23510a6b6
boydstacken <124229670+boydstacken@users.noreply.github.com>
2023-07-17 23:30:04 -0500

    added more code to routes

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,19 +1,49 @@
 const router = require('express').Router();
-
 const {getThoughts} = require("../../controllers/thoughts");
 
 router.route('/').get(getThoughts)
 
-router.post('/api/thoughts/:thoughtId/friends/:friendID', (req, res) => {
-    const user_id = req.params.thoughtId.indexOf
+router.get('/api/thoughts/:thoughtId', (req, res)=> {
+const thoughtId = req.params.thoughtId
 
+res.status(200).json({message:'Got single thought by id'})
 })
 
-router.delete('users/thoughtId/thought/:thought_id', (req, res) => {
-    thoughtId = req.params.thought_id
-    friendId = req.params.thought_id
+router.post('/api/thoughts ', (req, res) => {
+    const {userId, content} = req.body
+    const newThought = createThought(content)
+
+    res.status(200).json({message:'Posted to create a new thought'})
 })
 
-res.status(200).json({message: 'Friend removed'})
+router.put('/api/thoughts/:thoughtId', (req, res) => {
+    const thoughtId = req.params.thoughtId
+    const updatedContent = req.params.content
+    const updatedThought = updateThought(thoughtId, updatedContent)
 
+    res.status(200).json({message:'Updated a single thought by its id'})
+})
+
+router.delete('/api/thoughts/:thoughtId', (req, res) => {
+    const thoughtId = req.params.thoughtId
+    const deletedThought = removedThought(thoughtId)
+
+    res.status(200).json({message:'Deleted a single thought by its id'})
+})
+
+router.post('/api/thoughts/:thoughtId/reactions', (req, res) => {
+    const thoughtId = req.params.thoughtId
+    const {reaction} = req.body
+    const createdReaction = createReactionInThought(thoughtId, reaction)
+
+    res.status(200).json({message:'Created a reaction by its id'})
+})
+
+router.delete('/api/thoughts/:thoughtId/reactions', (req, res) => {
+    const thoughtId = req.params.thoughtId
+    const reactionId = req.params.reactionId
+
+    res.status(200).json({message:'Deleted a reaction by its id'})
+
+})
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,16 +5,40 @@ const {getUsers} = require("../../controllers/users");
 
 router.route('/').get(getUsers)
 
-router.post('/api/users/:userId/friends/:friendID', (req, res) => {
-    const user_id = req.params.userId.indexOf
+router.get('/api/users/:userId', async (req, res)=> {
+    const userId = req.params.userId
+    try {
+        const user = await user.findById(userId)
+        .populate('thoughts')
+        .populate('friends')
+    
+    res.status(200).json({message:'Got a single user by its id and populated thought and friend data', user});
+} catch (error) {
+    res.status(500).json({error: 'Failed to retreive user by its id with populated thought and friend data'});
+}
+});
 
+router.post('/api/users/', async (req, res) => {
+    try {
+        const newUser = await UserActivation.create(req.body)
+        
+        res.status(201).json({message: 'Created a new user'})
+    } catch (error) {
+    res.status(200).json({message: 'Error in creating a new user'})
+    }
 })
 
-router.delete('users/userId/friends/:friend_id', (req, res) => {
-    userId = req.params.user_id
-    friendId = req.params.friend_id
+router.put('/api/users/:userId/friends/:friendId', (req, res) => {
+    const userId = req.params.userId
+    res.status(200).json({message: 'Updated a user by its id'})
 })
 
-res.status(200).json({message: 'Friend removed'})
+router.delete('/api/users/userId/friends/:friendId', (req, res) => {
+    userId = req.params.userId
+    friendId = req.params.friendId
+
+    res.status(200).json({message: 'Friend removed'})
+
+})
 
 module.exports = router;
